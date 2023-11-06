02cf4b166a44e6ab01e206856d4a1cf45132c88d
nysdin <40791830+nysdin@users.noreply.github.com>
2023-11-07 00:37:11 +0900

    setup macos

---
+++ run_onchange_before_001_defaults.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -eux
+
+# dock: https://macos-defaults.com/dock/orientation.html
+defaults write com.apple.dock "orientation" -string bottom
+defaults write com.apple.dock "tilesize" -int "60"
+defaults write com.apple.dock "autohide" -bool "true"
+defaults write com.apple.dock "autohide-time-modifier" -float "0.5"
+defaults write com.apple.dock "autohide-delay" -float "0.1"
+defaults write com.apple.dock "show-recents" -bool "false"
+defaults write com.apple.dock "mineffect" -string "suck"
+killall Dock || true
+
+
+# Finder
+defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true"
+defaults write com.apple.finder "AppleShowAllFiles" -bool "true"
+defaults write com.apple.finder "_FXSortFoldersFirst" -bool "true"
+defaults write com.apple.finder "FXPreferredViewStyle" -string "clmv"
+killall Finder || true
+
+# Menu Bar
+defaults write com.apple.menuextra.clock "FlashDateSeparators" -bool "false"
+killall SystemUIServer
+
+# Trackpad
+defaults write com.apple.AppleMultitouchTrackpad "FirstClickThreshold" -int "0"
+
+# Appearance
+defaults write -g NSRequiresAquaSystemAppearance -bool Yes
